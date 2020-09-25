b77887c12f6204a6669c846bc3d73f1bada7f9f5
ismail <ismail.x@aol.com>
2020-09-25 20:12:41 +0700

    iniT

---
+++ galeriaa/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  galeriaa
+//
+//  Created by Ismail . on 25/09/20.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
---
+++ galeriaa/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  galeriaa
+//
+//  Created by Ismail . on 24/09/20.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
